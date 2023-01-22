75b9555ece3a7431bd96178fc6c12bf4d33976b1
JanCas <jan.l.cas@outlook.com>
2023-01-22 16:35:33 -0700

    take out some unecessary matrices and convert them to simple doubles

---
+++ include/computational/ThermalPower.h
@@ -0,0 +1,7 @@
+#ifndef THERMALPOWER_H
+#define THERMALPOWER_H
+
+void spectral_total_conductance_thermal_power();
+
+
+#endif /* THERMALPOWER_H */
--- src/DSGF_main.c
+++ src/DSGF_main.c
@@ -102,10 +102,8 @@ int main()
 	double (*G_12_omega_SGF)[N_Tcalc] = calloc(const_N_omega, sizeof(*G_12_omega_SGF));
 
 	double (*modulo_r_i_j)[tot_sub_vol] = malloc(sizeof *modulo_r_i_j * tot_sub_vol);
-	double complex (*G_element)[tot_sub_vol] = malloc(sizeof *G_element * tot_sub_vol); 
-	double (*sum_trans_coeff) = malloc(sizeof *sum_trans_coeff *const_N_omega); 
+	double (*sum_trans_coeff) = calloc(const_N_omega, sizeof(*sum_trans_coeff)); 
 
-	double complex (*Q_omega_subvol) = malloc(sizeof * Q_omega_subvol * tot_sub_vol); // power dissipated per subvolume
 	double (*Q_omega_thermal_object)[const_N_omega] = malloc(sizeof * Q_omega_thermal_object * const_N_bulk_objects);  
 	double (*Q_subvol)[const_N_omega] = calloc(tot_sub_vol, sizeof(*Q_subvol));
 
@@ -295,18 +293,12 @@ int main()
 		// ###################  Thermal power dissipated ###################
 		// #################################################################
 
-		double complex (*trans_coeff)[tot_sub_vol] = calloc(tot_sub_vol, sizeof(*trans_coeff));
-
-		double complex (*G_sys_cross)[tot_sub_vol][3][3] = calloc(tot_sub_vol, sizeof(*G_sys_cross)); 
-		double complex (*transpose_G_sys)[tot_sub_vol][3][3] = calloc(tot_sub_vol, sizeof(*transpose_G_sys)); 
-		double(*inner_sum) = malloc(sizeof * inner_sum * tot_sub_vol);
-
-		sum_trans_coeff[i_omega] = 0.;    
-
 		for (int ig_0 = 0; ig_0 < tot_sub_vol; ig_0++) //tot_sub_vol
 		{
+			double complex Q_omega_subvol = 0;
 			for (int jg_0 = 0; jg_0 < tot_sub_vol; jg_0++) //tot_sub_vol
 			{
+				double complex G_element = 0;
 				for(int i_subG_0 = 0; i_subG_0 < 3; i_subG_0++) // 3D coordinate positions
 				{
 					int ig_0_2d = (3*ig_0 + i_subG_0); // Set indices
@@ -314,54 +306,49 @@ int main()
 					{
 						int jg_0_2d = (3*jg_0 + j_subG_0); // Set indices
 						// Extract one Green's function component: G_sys[ig_0][jg_0][i_subG_0]
-						transpose_G_sys[jg_0][ig_0][i_subG_0][j_subG_0] = G_sys[ig_0_2d][jg_0_2d];
-						G_sys_cross[ig_0][jg_0][i_subG_0][j_subG_0]= conj(transpose_G_sys[jg_0][ig_0][i_subG_0][j_subG_0]);
-						G_element[ig_0][jg_0]+=G_sys[ig_0_2d][jg_0_2d]*G_sys_cross[ig_0][jg_0][i_subG_0][j_subG_0]; 
+						double complex transpose_G_sys = G_sys[ig_0_2d][jg_0_2d];
+						double complex G_sys_cross = conj(transpose_G_sys);
+						G_element+=G_sys[ig_0_2d][jg_0_2d]*G_sys_cross; 
 
 					}    
 				}
 				// Transmissivity coefficient matrix tau(omega) for comparison with Czapla Mathematica output [dimensionless]
-				trans_coeff[ig_0][jg_0] = 4.*pow(k_0,4)*delta_V_vector[ig_0]*delta_V_vector[jg_0]*cimag(epsilon)*cimag(epsilon)*G_element[ig_0][jg_0] ;  
+				double complex trans_coeff = 4.*pow(k_0,4)*delta_V_vector[ig_0]*delta_V_vector[jg_0]*cimag(epsilon)*cimag(epsilon)*G_element; 
+				double inner_sum;
 				// Thermal power dissipated calculation, based on the matlab code (Using Tervo's Eq. 26)
 				if (ig_0 != jg_0) 
 				{
-					inner_sum[jg_0] = (theta_function(omega_value, T_vector[jg_0], h_bar, k_b) - theta_function(omega_value, T_vector[ig_0], h_bar, k_b)) * trans_coeff[ig_0][jg_0];
+					inner_sum = (theta_function(omega_value, T_vector[jg_0], h_bar, k_b) - theta_function(omega_value, T_vector[ig_0], h_bar, k_b)) * trans_coeff;
 				}
 				else {
-					inner_sum[jg_0] = 0;
+					inner_sum = 0;
 				}
 
 				//Trans_bulk: Transmission coefficient between two bulk objects
 				// This function calculates the transmission coefficient between bulk objects given the transmission coefficient between every pair of dipoles for a given frequency.
 				if(ig_0 < const_N_subvolumes_per_object && jg_0 >= const_N_subvolumes_per_object)// bulk 1 -> 2
 				{
-					sum_trans_coeff[i_omega] += trans_coeff[ig_0][jg_0];
+					sum_trans_coeff[i_omega] += trans_coeff;
 				} 
 
-				Q_omega_subvol[ig_0] += (1 / (2 * pi)) * inner_sum[jg_0]; // calculates the thermal power dissipated per subvolume
+				Q_omega_subvol += (1 / (2 * pi)) * inner_sum; // calculates the thermal power dissipated per subvolume
 
 			} 
-			Q_subvol[ig_0][i_omega] = Q_omega_subvol[ig_0];
+			Q_subvol[ig_0][i_omega] = Q_omega_subvol;
 
 			if(ig_0 < const_N_subvolumes_per_object)// Thermal power dissipated was not verified yet!!!!
 			{
 				int bulk=0;
-				Q_omega_thermal_object[bulk][i_omega] += Q_omega_subvol[ig_0];
+				Q_omega_thermal_object[bulk][i_omega] += Q_omega_subvol;
 			}
 			else if(const_N_subvolumes_per_object<=ig_0<tot_sub_vol)
 			{
 				int bulk=1;
-				Q_omega_thermal_object[bulk][i_omega] += Q_omega_subvol[ig_0];
+				Q_omega_thermal_object[bulk][i_omega] += Q_omega_subvol;
 			} 
 
 		}       
 		free(G_sys);
-
-		free(trans_coeff);
-		free(G_sys_cross);
-		free(transpose_G_sys);
-
-		free(inner_sum);
 
 
 		if(save_spectral_transmissivity =='Y'){
@@ -405,9 +392,7 @@ int main()
 		// #############  Clear values for next frequency ##################
 		// #################################################################
 
-		memset(G_element, 0, sizeof *G_element * tot_sub_vol); //G_element
 		memset(sum_trans_coeff, 0, sizeof sum_trans_coeff);
-		memset(Q_omega_subvol, 0, sizeof* Q_omega_subvol* tot_sub_vol); 
 
 	} // END OMEGA VALUE LOOP FOR FREQUENCY RANGE ANALYSIS
 
@@ -415,7 +400,6 @@ int main()
 	free(sum_trans_coeff);
 	free(modulo_r_i_j);
 	free(r_i_j_outer_r_i_j);
-	free(G_element);
 
 	free(alpha_0);
 
@@ -508,7 +492,6 @@ int main()
 
 	free(trapz_Q);
 	free(Q_subvol);
-	free(Q_omega_subvol);
 	free(Q_omega_thermal_object);
 	free(results_folder);
 
