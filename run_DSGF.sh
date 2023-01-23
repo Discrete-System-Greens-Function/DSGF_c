48c0c78df6c3cc5a32cd6820de30c401553a5feb
JanCas <jan.l.cas@outlook.com>
2023-01-23 09:45:15 -0700

    new compilation script

--- run_DSGF.sh
+++ run_DSGF.sh
@@ -21,13 +21,11 @@ echo "Compilation in process..."
 
 export OMP_THREADS=4 # use multiple cores during the calculation
 
-icc -O3  -std=c99 -I include/ src/array_functions.c src/geometry/sphere.c src/geometry/thin_film.c src/material/u_SiC.c src/material/SiN.c src/material/SiO2.c src/material/SiC.c src/DSGF_main.c src/file_utils.c src/functions_DSGF.c src/iterative_solver.c -o DSGF -mcmodel=medium  -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl  -I"${MKLROOT}/include" # -openmp 
+mpiicc -Ofast -qopenmp -Wall -std=c99 -I include/ src/computational/GreensFunction.c src/array_functions.c src/geometry/sphere.c src/geometry/thin_film.c src/material/u_SiC.c src/material/SiN.c src/material/SiO2.c src/material/SiC.c src/DSGF_main.c src/file_utils.c src/functions_DSGF.c src/computational/solvers/iterative_solver.c src/computational/solvers/direct_solver.c src/computational/ThermalPower.c src/geometry/shared.c -o DSGF -mcmodel=medium  -L${MKLROOT}/lib/intel64 -lmkl_intel_lp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -ldl  -I"${MKLROOT}/include" # -openmp 
  
 
 echo "Compilation complete"
 #  MALLOC_sgf-sphere-lapacke-linux.c  sgf-flat-surfaces-lapacke-linux.c  sgf-lapacke_original.c or sgf-lapacke-linux.c sgf-sphere-lapacke-linux.c sgf-flat-surfaces-lapacke-linux_v2.c  MALLOC_sgf-sphere-lapacke-linux    
-
-time ./DSGF  #time function displays the real, user and system time for running the code;    /.dsgf_LAPACKE_9N2 runs the compiled code. 
 
 # Improvement MALLOC:
 # https://stackoverflow.com/questions/13534966/how-to-dynamically-allocate-a-contiguous-block-of-memory-for-a-2d-array
