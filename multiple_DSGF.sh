2f88c44a2bb65bc4046fc797fe3622ee35e3a5df
Livia Correa <44705597+liviacorrea@users.noreply.github.com>
2023-06-27 12:03:36 -0600

    Add files via upload

---
+++ multiple_DSGF.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "simulation 1"
+./DSGF
+sleep 5
+
+echo "simulation 2"
+./DSGF
+sleep 5
+
+echo "simulation 3"
+./DSGF
+sleep 5
+
+echo "simulation 4"
+./DSGF
+sleep 5
+
+echo "simulation 5"
+./DSGF
